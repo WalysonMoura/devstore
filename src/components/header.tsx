aa8d1608e9456112c8e3874ea1605984a08ac4b4
{ Walyson } <walysonmoura222@gmail.com>
2024-01-16 18:13:01 +0000

    new

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,30 @@
-
+import Image from "next/image";
+import Link from "next/link";
+import { Suspense } from "react";
 
 export function Header() {
-    return(
-        <div className="flex ite"></div>
-    )
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <Link href="/">devstore</Link>
+
+        <Suspense fallback={null}></Suspense>
+      </div>
+
+      <div>
+        <div className="w-px h-4 bg-zinc-700"></div>
+
+        <Link href="/">
+          <span className="text-sm">Account</span>
+          <Image
+            src="https://github.com/WalysonMoura.png"
+            alt=""
+            className="h-6 w-6 rounded-full"
+            width={24}
+            height={24}
+          />
+        </Link>
+      </div>
+    </div>
+  );
 }
